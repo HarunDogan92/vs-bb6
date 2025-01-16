7de5b305eb3259ae078860f09831f131e69da057
Enes Baysal <enesbaysal_68@hotmail.com>
2025-01-16 02:02:09 +0100

    lil refactor

--- ds-finance-bank-web/src/main/java/net/froihofer/bean/BankBean.java
+++ ds-finance-bank-web/src/main/java/net/froihofer/bean/BankBean.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.ExternalContext;
 import jakarta.inject.Named;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
@@ -31,11 +32,9 @@ public class BankBean implements Serializable {
     public void searchStocks() {
         try {
             searchResults = customerService.searchStocks(searchQuery);
-            System.out.println(searchResults.get(0).getSymbol());
         } catch (Exception e) {
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "fehler beim suchen", e.getMessage()));
-            e.printStackTrace();
         }
     }
 
@@ -72,16 +71,15 @@ public class BankBean implements Serializable {
 
     public void logout() {
         try {
-            System.out.println("Logout called");
-            FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
             FacesContext context = FacesContext.getCurrentInstance();
+            context.getExternalContext().invalidateSession();
             context.getExternalContext().setResponseHeader("Cache-Control", "no-cache, no-store, must-revalidate");
             context.getExternalContext().setResponseHeader("Pragma", "no-cache");
             context.getExternalContext().setResponseHeader("Expires", "0");
             context.getExternalContext().responseSendError(401, "Unauthorized");
             context.responseComplete();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
